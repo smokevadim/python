fb65aac29910095c49d4206ad8521ae9a6f4a070
smokevadim <52584374+smokevadim@users.noreply.github.com>
2019-08-14 14:50:32 +0200

    1

--- Django-test/django_test_server/polls/views.py
+++ Django-test/django_test_server/polls/views.py
@@ -7,12 +7,12 @@ from django.urls import reverse
 # Create your views here.
 
 def detail(request, question_id):
-        question = get_object_or_404(Question, pk=question_id)
-        return render(request,'polls/detail.html', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request,'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request,'polls:results.html', {'question': question})
+    return render(request,'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
